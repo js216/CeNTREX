js216/CeNTREX

commit 5661dfb8df6687d4f7b0d88000b5b11e07ace9fc

add InfluxDB_enabled default option to devices

main.py
@@ -806,6 +806,7 @@ class DeviceConfig(Config):
         super().__init__()
         self.fname = config_fname
         self.define_permitted_keys()
+        self.set_defaults()
         self.read_from_file()
 
     def define_permitted_keys(self):
@@ -824,6 +825,7 @@ class DeviceConfig(Config):
                 "plots_queue_maxlen" : int,
                 "max_NaN_count"      : int,
                 "meta_device"        : bool,
+                "InfluxDB_enabled"   : bool,
             }
 
         # list of keys permitted for runtime data (which cannot be written to .ini file)
@@ -844,6 +846,9 @@ class DeviceConfig(Config):
                 "attributes"     : dict,
                 "control_params" : dict,
             }
+
+    def set_defaults(self):
+        self["InfluxDB_enabled"] = True
 
     def change_param(self, key, val, sect=None, sub_ctrl=None, row=None):
         if row != None:

commit 2ae79d22fe309750cc198255d9cc3d10e49f9b1e

fix bugs introduced by 5661dfb8df6687d4f7b0d88000b5b11e07ace9fc

main.py
@@ -873,7 +873,14 @@ class DeviceConfig(Config):
 
         # read general device options
         for key, typ in self.static_keys.items():
+            # read a parameter from the .ini file
             val = params["device"].get(key)
+
+            # check the parameter is defined in the file; leave it at its default value if not
+            if not val:
+                continue
+
+            # if the parameter is defined in the .init file, parse it into correct type:
             if typ == list:
                 self[key] = [x.strip() for x in val.split(",")]
             elif typ == bool:
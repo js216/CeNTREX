6bf861e4e1ca091d3886c036ba59477d8b0da2e1
ograsdijk <o.grasdijk@gmail.com>
2019-11-29 18:28:22 -0500

    Added driver for simultaneous control of the fiber swich and wavemeter

---
+++ drivers/WavemeterFiberSwitch.py
@@ -0,0 +1,58 @@
+from drivers.Bristol671A import *
+from drivers.SelfAlignFiberSwitch import *
+
+class WavemeterFiberswitch:
+    def __init__(self, time_offset, connection_wavemeter, COM_port_fiberswitch, ports):
+        self.time_offset    = time_offset
+        self.wavemeter      = Bristol671A(time_offset, connection_wavemeter)
+        self.switch         = SelfAlignFiberSwitch(time_offset, COM_port_fiberswitch)
+
+        self.verification_string = self.wavemeter.verification_string + ', '
+        self.verification_string += self.switch.verification_string
+
+        self.ports = [int(p) for p in ports.split(',')]
+
+        self.dtype = tuple(['f4'] * (len(self.ports)+1))
+        self.shape = (1+len(self.ports),)
+
+        self.new_attributes = []
+        self.warnings = []
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *exc):
+        self.wavemeter.__exit__(exc)
+        self.switch.__exit__(exc)
+
+    def GetWarnings(self):
+        warnings = self.warnings.copy()
+        warnings.extend(self.wavemeter.GetWarnings())
+        warnings.extend(self.switch.GetWarnings())
+        self.warnings = []
+        return warnings
+
+    def ReadValue(self):
+        port = self.switch.ReadValue()
+        t, frequency = self.wavemeter.ReadValue()
+        frequencies = [np.nan]*len(self.ports)
+
+        if isinstance(port, (list, tuple)):
+            port = port[1]
+            frequencies[self.ports.index(port)] = frequency
+
+        return [t]+frequencies
+
+    def SetPort(self,port):
+        try:
+            port = int(port)
+        except Exception as err:
+            logging.warning("SelfAlignFiberSwitch warning in Setport : can't convert {0} to int".format(port))
+            warning_dict = { "Setport; can't convert {0} to int".format(port)}
+            self.warnings.append([time.time(), warning_dict])
+        if port in self.ports:
+            self.switch.SetPort(port)
+        else:
+            logging.warning("WavemeterFiberswitch warning in Setport : port {0} not connected".format(port))
+            warning_dict = { "Setport; can't convert {0} to int".format(port)}
+            self.warnings.append([time.time(), warning_dict])

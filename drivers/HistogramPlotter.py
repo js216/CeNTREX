45a5cf0f666540aa6064ba81e712b3b1d32dbd38
ograsdijk <o.grasdijk@gmail.com>
2020-03-13 19:25:47 -0400

    Adding documentation to HistogramPlotter

--- drivers/HistogramPlotter.py
+++ drivers/HistogramPlotter.py
@@ -6,25 +6,33 @@ def split(string, separator=","):
     return [x.strip() for x in string.split(separator)]
 
 class HistogramPlotter:
+    """
+    Driver takes data from a fast device and a slow device, processes a trace
+    from a fast device and subsequently bins the processed data and slow device
+    data together, enabling plotting of histograms.
+    """
     def __init__(self, parent, time_offset, *params):
         self.parent = parent
         self.time_offset = time_offset
         self.dev1, self.param1, self.processing, self.dev2, self.param2, \
         self.lower, self.higher, self.width = params
+
+        # lower and higher bounds for the bin range
         self.lower = float(self.lower)
         self.higher = float(self.higher)
+        # bin width
         self.width = float(self.width)
 
         self.verification_string = 'test'
 
+        # lists to store matched slow (x) and processed fast (y) data in
         self.x_data = []
         self.y_data = []
 
-        self.dev1_time = 0
-        self.dev2_time = 0
-
+        # storing a hash to ensure only new fast data is added to the histogram
         self.dev1_hash = None
 
+        # creating the bin edges
         self.bins = np.arange(self.lower, self.higher+self.width, self.width)
 
         self.warnings = []
@@ -44,12 +52,16 @@ class HistogramPlotter:
         return []
 
     def ReadValue(self):
+        """
+        Binning the fast and slow data to enable plotting of histograms
+        """
         self.ProcessData()
         x_data = self.x_data
         y_data = np.array(self.y_data)
+        bins = self.bins
+
         if (len(x_data) == 0) or (len(y_data) == 0):
             return np.nan
-        bins = self.bins
 
         bin_indices = np.digitize(x_data, bins)
         bin_means = np.array([y_data[bin_indices == i].mean() for i in range(1,len(bins))])
@@ -57,6 +69,9 @@ class HistogramPlotter:
         return [data, [{'timestamp': time.time() - self.time_offset}]]
 
     def FetchData(self):
+        """
+        Attempting to fetch data from the specified fast and slow device.
+        """
         try:
             data1 = self.parent.devices[self.dev1].config["plots_queue"][-1]
             data2 = self.parent.devices[self.dev2].config["plots_queue"][-1]
@@ -64,7 +79,7 @@ class HistogramPlotter:
             logging.error('HistogramPlotter error in FetchData() : IndexError')
             return
 
-        # extract the desired parameter 1
+        # extract the desired parameter 1 and 2
         col_names1 = split(self.parent.devices[self.dev1].config["attributes"]["column_names"])
         col_names2 = split(self.parent.devices[self.dev2].config["attributes"]["column_names"])
         try:
@@ -80,19 +95,23 @@ class HistogramPlotter:
         return param1_dset, param2_val
 
     def ProcessData(self):
+        """
+        Processing data from the fast device
+        """
         data = self.FetchData()
         if data is None:
             logging.warning('Warning in HistogramPlotter ProcessData() : no data retrieved')
             return
 
         y, param2_val = data
-        dev1_hash = hash(y[:10].tostring())
 
+        # checking if fast device data has already been acquired before
+        dev1_hash = hash(y[:10].tostring())
         if dev1_hash == self.dev1_hash:
             return
-
         self.dev1_hash = dev1_hash
 
+        # evaluating the processing string supplied to __init__
         processed = eval(self.processing)
 
         self.x_data.append(param2_val)

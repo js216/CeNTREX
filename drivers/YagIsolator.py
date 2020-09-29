553881a1ab5026064e2d3911c14de746091cc01a
Olivier Grasdijk <o.grasdijk@gmail.com>
2020-09-29 18:52:58 -0400

    YAGisolator driver added to readout

--- drivers/YagIsolator.py
+++ drivers/YagIsolator.py
@@ -6,20 +6,22 @@ class YagIsolator:
     def __init__(self, time_offset, resource_name):
         self.time_offset = time_offset
         self.rm = pyvisa.ResourceManager()
-        try:
-            self.instr = self.rm.open_resource(resource_name)
-        except pyvisa.errors.VisaIOError as err:
-            self.verification_string = str(err)
-            self.instr = False
-            return
-        self.instr.baud_rate = 9600
-        self.instr.data_bits = 8
-        self.instr.parity = pyvisa.constants.Parity.none
-        self.instr.stop_bits = pyvisa.constants.StopBits.one
-        self.instr.read_termination = "\r\n"
 
-        # make the verification string
-        self.ClearBuffer()
+        if resource_name != 'client':
+            try:
+                self.instr = self.rm.open_resource(resource_name)
+            except pyvisa.errors.VisaIOError as err:
+                self.verification_string = str(err)
+                self.instr = False
+                return
+            self.instr.baud_rate = 9600
+            self.instr.data_bits = 8
+            self.instr.parity = pyvisa.constants.Parity.none
+            self.instr.stop_bits = pyvisa.constants.StopBits.one
+            self.instr.read_termination = "\r\n"
+
+            # make the verification string
+            self.ClearBuffer()
         self.verification_string = self.QueryIdentification()
 
         # HDF attributes generated when constructor is run
@@ -33,7 +35,7 @@ class YagIsolator:
 
         # convenience attributes to cut down on serial communication for
         # monitoring commands
-        self.qswitch_status = bool(self.instr.query("status D"))
+        self.qswitch_status = False
         self.nr_qswitches = 0
 
     def __enter__(self):
@@ -42,9 +44,9 @@ class YagIsolator:
     def __exit__(self, *exc):
         if self.instr:
             self.instr.close()
-
+            
     def ReadValue(self):
-        return [ 
+        return [
                 time.time()-self.time_offset
                ]
 
@@ -79,7 +81,7 @@ class YagIsolator:
     def SetNrQswitchesGUI(self, nr_qswitches):
         self.NrQswitches(nr_qswitches)
         self.nr_qswitches = nr_qswitches
-    
+
     def GetNrQswitchesGUI(self, nr_qswitches):
         return self.nr_qswitches
 
@@ -104,7 +106,7 @@ class YagIsolator:
         ret = self.instr.query(cmd)
         if ret != cmd:
             logging.warning("YagIsolator warning in QswitchCounter(): "+ret)
-    
+
     def Enable(self, channel):
         assert channel in ['C', 'D'], f'invalid channel : {channel}'
         cmd = f'enable {channel}'

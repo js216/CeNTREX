42a54f4ae499722f6cea94ade6fa6245082bac9e
ograsdijk <o.grasdijk@gmail.com>
2020-12-09 23:46:27 -0500

    Functional networkclient

--- drivers/NetworkingClient.py
+++ drivers/NetworkingClient.py
@@ -2,7 +2,12 @@ import zmq
 import json
 import time
 import logging
+import inspect
+import threading
+import importlib
+import functools
 import numpy as np
+from types import FunctionType
 
 def wrapperNetworkClientMethods(func):
     """
@@ -24,7 +29,7 @@ def NetworkingClassDecorator(cls):
     """
     # don't wrap methods with this name
     ignore = ['__init__', '__enter__', '__exit__', 'OpenConnection', 
-            'CloseConnection', 'ExecuteNetworkCommand', 'ReadValue', 
+            'CloseConnection', 'ExecuteNetworkCommand', 'ReadValue', 'Decode',
             'GetWarnings']
     for attr_name in dir(cls):
         attr_value = getattr(cls, attr_name)
@@ -40,8 +45,56 @@ def NetworkingClassDecorator(cls):
                 setattr(cls, attr_name, attribute)
     return cls
 
-def NetworkingClient(time_offset, connection, device, *args):
-    driver = args[2]
+class ReadValueThread(threading.Thread):
+    def __init__(self, parent):
+        super(ReadValueThread, self).__init__()
+        self.parent = parent
+        self.value = None
+        self.daemon = True
+        self.active = threading.Event()
+
+    def run(self):
+        """
+        Keep listening on the readout port for new messages from the
+        server in a separate thread, such that the parent class can issue
+        control commands while waiting for a message.
+        """
+        while self.active.is_set():
+            # wrap in try-except because the context is sometimes terminated 
+            # upon stopping the program before the while loop reaches the 
+            # cleared thread event active
+            try:
+                data = self.parent.socket_readout.recv_string()
+                retval = self.parent.Decode(data)
+                retval[0] -= self.parent.time_offset
+                self.value = retval
+                # need a sleep to release to other threads
+                time.sleep(1e-4)
+            except zmq.error.ContextTerminated:
+                logging.info(f"{self.parent.device_name} networking info in " +
+                                    f"ReadValueThread : stopped subscription")
+                pass
+
+
+
+def NetworkingClient(time_offset, driver, connection, *args):
+    # if connection is passed as a string, convert it to a dictionary
+    if isinstance(connection, str):
+        connection = json.loads(connection)
+
+    # very hacky way to get shape and dtype from the original driver
+    # this is defined in __init__ of the driver and as such you need to 
+    # initialize the class to access it, which throws exceptions for some 
+    # drivers if a usb/serial/whatever device is not attached
+    with open(f'drivers/{driver}.py') as f:
+        text = f.readlines()
+    
+    for line in text:
+        if 'self.dtype' in line:
+            dtype = line.split('=')[1].strip()
+        if 'self.shape' in line:
+            shape = line.split('=')[1].strip()
+
     driver_spec = importlib.util.spec_from_file_location(
         driver,
         "drivers/" + driver + ".py",
@@ -51,12 +104,12 @@ def NetworkingClient(time_offset, connection, device, *args):
     driver_spec.loader.exec_module(driver_module)
     driver = getattr(driver_module, driver)
 
+
     @NetworkingClassDecorator
     class NetworkingClientClass(driver):
-        def __init__(self, time_offset, device, connection, *args):
+        def __init__(self, time_offset, connection, dtype, shape, *args):
             self.time_offset = time_offset
-            self.device = device
-            
+
             self.server = connection['server']
             self.port_readout = connection['port_readout']
             self.port_control = connection['port_control']
@@ -66,13 +119,29 @@ def NetworkingClient(time_offset, connection, device, *args):
             # set the control connection timeout
             self.timeout = 10e3
             # open connections to the server
+            self.topicfilter = f'{self.publisher}-{self.device_name}'
             self.OpenConnection()
 
             self.warnings = []
 
-            self.args = [eval(_) for _ in args]
-        
-        def GetWarnings():
+            self.readvalue_thread = ReadValueThread(self)
+            self.readvalue_thread.active.set()
+            self.readvalue_thread.start()
+
+            self.verification_string = 'network'
+
+            self.dtype = eval(dtype)
+            self.shape = eval(shape)
+
+        def __exit__(self, *args):
+            self.readvalue_thread.active.clear()
+            self.socket_readout.setsockopt(zmq.LINGER, 0)
+            self.socket_control.setsockopt(zmq.LINGER, 0)
+            self.socket_readout.close()
+            self.socket_control.close()
+            self.context.term()
+
+        def GetWarnings(self):
             return self.warnings
 
         def OpenConnection(self):
@@ -80,13 +149,11 @@ def NetworkingClient(time_offset, connection, device, *args):
             self.context = zmq.Context()
             # opening the sockets
             self.socket_control = self.context.socket(zmq.REQ)
-            self.socket_readout = self.context.socket(zmq.PUB)
+            self.socket_readout = self.context.socket(zmq.SUB)
 
             # starting readout
-            topicfilter = f"{self.publisher}-{self.device_name}"
-            self.socket_readout.setsockopt_string(zmq.SUBSCRIBE, topicfilter)
-            # only keep the last value from the publisher in the queue
-            self.socket_readout.socket.setsockopt(zmq.CONFLATE, 1)
+            self.socket_readout.setsockopt_string(zmq.SUBSCRIBE, 
+                                                    self.topicfilter)
             self.socket_readout.connect(f"tcp://{self.server}:{self.port_readout}")
 
             # starting control
@@ -98,20 +165,18 @@ def NetworkingClient(time_offset, connection, device, *args):
             self.socket_readout.close()
             self.context.term()
         
-        @staticmethod
-        def decode(message):
+        def Decode(self, message):
             """
             Function decodes the message received from the publisher into a 
             topic and python object via json serialization
             """
-            json0 = message.find('{')
-            topic = message[0:json0].strip()
-            retval = json.loads(message[json0:])
-            return topic, retval
+            dat = message[len(self.topicfilter):]
+            retval = json.loads(dat)
+            return retval
 
         def ExecuteNetworkCommand(self, command): 
             # send command to the server hosting the device
-            self.socket_control.send_json([self.device, command])
+            self.socket_control.send_json([self.device_name, command])
 
             # need the timeout because REP-REQ will wait indefinitely for a 
             # reply, need to handle when a server stops or a message isn't
@@ -121,12 +186,12 @@ def NetworkingClient(time_offset, connection, device, *args):
                 if status == "OK":
                     return retval
                 else:
-                    logging.warning(f"{device} networking warning in " +
-                    +f"ExecuteNetworkCommand : error for {command} -> {retval}")
+                    logging.warning(f"{device_name} networking warning in " +
+                    f"ExecuteNetworkCommand : error for {command} -> {retval}")
                     return np.nan
 
             # error handling if no reply received withing timeout
-            logging.warning(f"{device} networking warning in " +
+            logging.warning(f"{device_name} networking warning in " +
                         +"ExecuteNetworkCommand : no response from server")
             self.socket_control.setsockopt(zmq.LINGER, 0)
             self.socket_control.close()
@@ -135,12 +200,9 @@ def NetworkingClient(time_offset, connection, device, *args):
             return np.nan
 
         def ReadValue(self):
-            topic, retval = self.decode(self.recv_string())
-            retval[0] -= time.time() - self.time_offset
-            return retval
-
-
-
-
+            if self.readvalue_thread.value:
+                return self.readvalue_thread.value
+            else:
+                return np.nan
     
-    return NetworkingClientClass(time_offset, connection, *args)
+    return NetworkingClientClass(time_offset, connection, dtype, shape, *args)

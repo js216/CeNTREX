14f0985366c4de4c1fe9e2e80368dac3d0efb1b4
ograsdijk <o.grasdijk@gmail.com>
2023-01-23 21:40:03 -0600

    Update SelfAlignFiberSwitch.py

--- drivers/SelfAlignFiberSwitch.py
+++ drivers/SelfAlignFiberSwitch.py
@@ -1,8 +1,10 @@
-import time
-import pyvisa
-import logging
 import functools
+import logging
+import time
+
 import numpy as np
+import pyvisa
+
 
 def CheckResponse(func):
     @functools.wraps(func)
@@ -10,32 +12,41 @@ def CheckResponse(func):
         try:
             if func(*args, **kwargs):
                 ret = args[0].instr.read_bytes(4)
-                if not b'\x01\x35\xff\xff' == ret:
-                    warning_dict = {"message": "invalid response in {0}".format(func.__name__)}
+                if not b"\x01\x35\xff\xff" == ret:
+                    warning_dict = {
+                        "message": "invalid response in {0}".format(func.__name__)
+                    }
                     args[0].warnings.append([time.time(), warning_dict])
-                    logging.warning("SelfAlignFiberSwitch warning in {0}() : ".format(func.__name__) \
-                                    +"invalid response")
+                    logging.warning(
+                        "SelfAlignFiberSwitch warning in {0}() : ".format(func.__name__)
+                        + "invalid response"
+                    )
         except pyvisa.errors.VisaIOError as err:
-                warning_dict = {"message": "Err in {0} : ".format(func.__name__)+str(err)}
-                args[0].warnings.append([time.time(), warning_dict])
-                logging.warning('SelfAlignFiberSwitch warning in {0}() : '.format(func.__name__) \
-                                +str(err))
+            warning_dict = {"message": "Err in {0} : ".format(func.__name__) + str(err)}
+            args[0].warnings.append([time.time(), warning_dict])
+            logging.warning(
+                "SelfAlignFiberSwitch warning in {0}() : ".format(func.__name__)
+                + str(err)
+            )
+
     return wrapper
+
 
 class SelfAlignFiberSwitch:
     def __init__(self, time_offset, COM_port):
         self.time_offset = time_offset
         self.rm = pyvisa.ResourceManager()
-        self.port = None
+        self.Home()
+        self.port = 1
         self.warnings = []
 
-        if COM_port not in ['client', ' ']:
+        if COM_port not in ["client", " "]:
             try:
                 self.instr = self.rm.open_resource(COM_port)
                 self.instr.parity = pyvisa.constants.Parity.none
                 self.instr.data_bits = 8
-                self.instr.write_termination = '\r\n'
-                self.instr.read_termination = '\r\n'
+                self.instr.write_termination = "\r\n"
+                self.instr.read_termination = "\r\n"
                 self.instr.baud_rate = 9600
                 self.verification_string = "True"
             except pyvisa.errors.VisaIOError:
@@ -47,9 +58,8 @@ class SelfAlignFiberSwitch:
         self.new_attributes = []
 
         # shape and type of the array of returned data
-        self.dtype = 'f4'
-        self.shape = (1, )
-
+        self.dtype = "f4"
+        self.shape = (1,)
 
     def __enter__(self):
         return self
@@ -64,7 +74,7 @@ class SelfAlignFiberSwitch:
 
     def ReadValue(self):
         if self.port:
-            return [time.time()-self.time_offset, self.port]
+            return [time.time() - self.time_offset, self.port]
         else:
             return np.nan
 
@@ -85,18 +95,23 @@ class SelfAlignFiberSwitch:
     def SetPort(self, port):
         try:
             port = int(port)
-        except Exception as err:
-            logging.warning("SelfAlignFiberSwitch warning in Setport : can't convert {0} to int".format(port))
-            warning_dict = { "Setport; can't convert {0} to int".format(port)}
+        except Exception:
+            logging.warning(
+                "SelfAlignFiberSwitch warning in Setport : can't convert {0} to int"
+                .format(port)
+            )
+            warning_dict = {"Setport; can't convert {0} to int".format(port)}
             self.warnings.append([time.time(), warning_dict])
         if self.port == port:
             return
         if (port > 16) or (port < 1):
-            logging.warning("SelfAlignFiberSwitch warning in Setport({0}) : ".format(port)\
-                            +"port out of range")
+            logging.warning(
+                "SelfAlignFiberSwitch warning in Setport({0}) : ".format(port)
+                + "port out of range"
+            )
             return
-        byte_value = bytes([port-1])
-        cmd = b'\x01\x35\x00'+byte_value
+        byte_value = bytes([port - 1])
+        cmd = b"\x01\x35\x00" + byte_value
         self.write(cmd)
         self.port = port
         return True

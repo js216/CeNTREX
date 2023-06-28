250d9c96d14f6048a665c3e8c0ea1d672256a284
ograsdijk <o.grasdijk@gmail.com>
2023-06-28 14:54:38 -0500

    update histogram plotter devices
    Now using timestamps to match closest scan value to data value

--- drivers/HistogramPlotter.py
+++ drivers/HistogramPlotter.py
@@ -1,13 +1,34 @@
 import logging
 import time
-import warnings
+from typing import Union
 
 import numpy as np
+import numpy.typing as npt
 from scipy.stats import binned_statistic
 
 
 def split(string, separator=","):
     return [x.strip() for x in string.split(separator)]
+
+
+def create_bins(
+    scan_values: Union[npt.NDArray[np.float_], npt.NDArray[np.int_]], maxsize: int = 100
+) -> tuple[npt.NDArray[np.float_], npt.NDArray[np.float_]]:
+    bin_centers = np.unique(scan_values)
+    if len(bin_centers) > maxsize:
+        bin_centers = np.linspace(bin_centers.min(), bin_centers.max(), maxsize)
+        bins = bin_centers.copy()
+        bin_width = bins[1] - bins[0]
+        bins -= bin_width / 2
+        bins = np.append(bins, bins.max() + bin_width / 2)
+
+    else:
+        dscan_min = np.diff(bin_centers).min()
+        bins = np.append(
+            [bin_centers.min() - dscan_min / 2],
+            bin_centers + dscan_min / 2,
+        )
+    return bin_centers, bins
 
 
 class HistogramPlotter:
@@ -26,10 +47,10 @@ class HistogramPlotter:
             self.processing,
             self.dev2,
             self.param2,
-            self.lower,
-            self.higher,
-            self.width,
+            self.nbins_max,
         ) = params
+
+        self.nbins_max = int(self.nbins_max)
 
         # Need the " marks surrounding the expression otherwise the CeNTREX DAQ
         # enter_cmd fails due to the eval inside main, this is a workaround
@@ -40,34 +61,23 @@ class HistogramPlotter:
         self.param2 = self.Strip(self.param2, '"')
         self.processing = self.Strip(self.processing, '"')
 
-        # lower and higher bounds for the bin range
-        self.lower = float(self.lower)
-        self.higher = float(self.higher)
-        # bin width
-        self.width = float(self.width)
+        self.verification_string = "histogram_plotter"
 
-        self.verification_string = "test"
-
-        # lists to store matched slow (x) and processed fast (y) data in
+        # lists to store matched slow (x), fast (unprocessed_data) and processed fast
+        # (y) data in
+        self.x_ts = []
         self.x_data = []
+        self.unprocessed_data_ts = []
+        self.unprocessed_data = []
         self.y_data = []
-
-        # storing a hash to ensure only new fast data is added to the histogram
-        self.dev1_hash = None
-
-        # creating the bin edges
-        self.bins = np.arange(self.lower, self.higher + self.width, self.width)
-
-        self.bin_centers = self.bins[:-1] + self.width / 2
-
         self.warnings = []
         self.new_attributes = []
 
         # shape and type of the array of returned data
-        self.shape = (1, 2, len(self.bins) - 1)
+        self.shape = (1, 2, self.nbins_max)
         self.dtype = float
 
-        self.no_data_err = False
+        self.processed_changed = False
 
     def __enter__(self):
         return self
@@ -96,48 +106,26 @@ class HistogramPlotter:
         self.ProcessData()
         x_data = self.x_data
         y_data = np.array(self.y_data)
-        bins = self.bins
 
         # return zeros if no data present
-        if (len(x_data)) == 0:
+        if len(x_data) == 0 or len(y_data) == 0 or len(np.unique(x_data)) <= 1:
             data = np.concatenate(
                 (np.linspace(-1, 1, self.shape[-1]), np.zeros(self.shape[-1]))
             ).reshape(self.shape)
             return [data, [{"timestamp": time.time() - self.time_offset}]]
 
-        if len(y_data) == 0:
-            data = np.concatenate(
-                (self.bin_centers, np.zeros(self.shape[-1]))
-            ).reshape(self.shape)
-            return [data, [{"timestamp": time.time() - self.time_offset}]]
-
-        try:
-            if np.diff(bins)[0] <= 0:
-                data = np.concatenate(
-                    (np.linspace(-1, 1, self.shape[-1]), np.zeros(self.shape[-1]))
-                ).reshape(self.shape)
-                return [data, [{"timestamp": time.time() - self.time_offset}]]
-
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", category=RuntimeWarning)
-                bin_means, bin_edges, bin_number = binned_statistic(
-                    x_data, y_data, statistic="mean", bins=bins
-                )
-
-            data = np.concatenate((self.bin_centers, bin_means)).reshape(
-                self.shape
+        else:
+            bin_centers, bins = create_bins(x_data, maxsize=self.nbins_max)
+            self.shape = (1, 2, len(bin_centers))
+            bin_means, bin_edges, bin_number = binned_statistic(
+                x_data, y_data, statistic="mean", bins=bins
             )
+            data = np.concatenate((bin_centers, bin_means)).reshape(self.shape)
 
-            return [data, [{"timestamp": time.time() - self.time_offset}]]
-        except Exception as e:
-            data = np.concatenate(
-                (np.linspace(-1, 1, self.shape[-1]), np.zeros(self.shape[-1]))
-            ).reshape(self.shape)
             return [data, [{"timestamp": time.time() - self.time_offset}]]
 
     def SetProcessing(self, processing):
         self.processing = processing
-        self.ClearData()
 
     def SetDevice1(self, dev1):
         self.dev1 = dev1
@@ -155,33 +143,15 @@ class HistogramPlotter:
         self.param2 = param2
         self.ClearData()
 
-    def SetLower(self, lower):
-        self.lower = lower
-        self.SetBins()
-
-    def SetHigher(self, higher):
-        self.higher = higher
-        self.SetBins()
-
-    def SetWidth(self, width):
-        self.width = width
-        self.SetBins()
+    def set_nbins_max(self, nbins_max: int):
+        self.nbins_max = int(nbins_max)
 
     def ClearData(self):
+        self.x_ts = []
         self.x_data = []
+        self.unprocessed_data_ts = []
+        self.unprocessed_data = []
         self.y_data = []
-        self.dev1_hash = None
-        self.no_data_err = False
-
-    def SetBins(self):
-        if self.lower >= self.higher:
-            logging.error(
-                "Error in HistogramPlotter: lower bin is larger than higher bin"
-            )
-            return
-        self.bins = np.arange(self.lower, self.higher + self.width, self.width)
-        self.bin_centers = self.bins[:-1] + (self.bins[1] - self.bins[0]) / 2
-        self.shape = (1, 2, len(self.bins) - 1)
 
     #################################################
     # Device Commands
@@ -192,10 +162,35 @@ class HistogramPlotter:
         Attempting to fetch data from the specified fast and slow device.
         """
         try:
-            data1 = self.parent.devices[self.dev1].config["plots_queue"][-1]
-            data2 = self.parent.devices[self.dev2].config["plots_queue"][-1]
-        except IndexError:
+            data1_queue = self.parent.devices[self.dev1].config["plots_queue"]
+        except KeyError:
+            logging.warning(f"HistogramPlotterNorm: device {self.dev1} not found")
             return
+        try:
+            data2_queue = np.asarray(
+                self.parent.devices[self.dev2].config["plots_queue"]
+            )
+        except KeyError:
+            logging.warning(f"HistogramPlotterNorm: device {self.dev} not found")
+            return
+
+        if len(data2_queue) == 0 or len(data1_queue) == 0:
+            return
+
+        timestamps1 = np.asarray([d[-1][0]["timestamp"] for d in data1_queue])
+        timestamps2 = np.asarray([d[0] for d in data2_queue])
+
+        dt = timestamps2[:, np.newaxis] - timestamps1
+        dt[dt > 0] = 1e3
+        indices2 = np.argmin(np.abs(dt), axis=0)
+
+        timestamps2 = timestamps2[indices2]
+        data2_queue = data2_queue[indices2]
+
+        if len(self.unprocessed_data) == 0:
+            mask = np.ones(len(data1_queue), dtype=bool)
+        else:
+            mask = timestamps1 > self.unprocessed_data_ts[-1]
 
         # extract the desired parameter 1 and 2
         col_names1 = split(
@@ -205,41 +200,67 @@ class HistogramPlotter:
             self.parent.devices[self.dev2].config["attributes"]["column_names"]
         )
         try:
-            param1_dset = data1[0][0, col_names1.index(self.param1)].astype(float)
+            idx1 = col_names1.index(self.param1)
         except IndexError:
             logging.error("Error in HistogramPlotter: param not found: " + self.param1)
             return
         try:
-            param2_val = float(data2[col_names2.index(self.param2)])
+            idx2 = col_names2.index(self.param2)
         except IndexError:
             logging.error("Error in HistogramPlotter: param not found: " + self.param2)
             return
-        return param1_dset, param2_val
+
+        self.unprocessed_data_ts = np.append(
+            self.unprocessed_data_ts, timestamps1[mask]
+        )
+        self.x_ts = np.append(self.x_ts, timestamps2[mask])
+
+        data1_queue = [data1_queue[idx] for idx, m in enumerate(mask) if m]
+        if len(self.unprocessed_data) == 0:
+            self.unprocessed_data = np.asarray(
+                [d[0][0][idx1] for d in data1_queue]
+            ).astype(float)
+            self.x_data = np.asarray([d[idx2] for d in data2_queue[mask]])
+        else:
+            for idd, d in enumerate(data1_queue):
+                # d is a list with at 0 the arrays and at 1 the timestamps
+                d = d[0]
+                if d.shape[0] > 1:
+                    for di in d:
+                        self.unprocessed_data = np.vstack(
+                            [self.unprocessed_data, di[0][idx1]]
+                        ).astype(float)
+                        self.x_data = np.append(
+                            self.x_data, data2_queue[mask][idd][idx2]
+                        ).astype(float)
+                else:
+                    self.unprocessed_data = np.vstack(
+                        [self.unprocessed_data, d[0][idx1]]
+                    ).astype(float)
+                    self.x_data = np.append(
+                        self.x_data, data2_queue[mask][idd][idx2]
+                    ).astype(float)
 
     def ProcessData(self):
         """
         Processing data from the fast device
         """
-        data = self.FetchData()
-        if data is None:
-            if not self.no_data_err:
-                logging.warning(
-                    "Warning in HistogramPlotter ProcessData() : no data retrieved"
-                )
-                self.no_data_err = True
+        self.FetchData()
+
+        if self.processed_changed:
+            self.y_data = []
+            for y in self.unprocessed_data:
+                self.y_data.append(eval(self.processing))
+            self.processed_changed = False
             return
-        self.no_data_err = False
 
-        y, param2_val = data
-
-        # checking if fast device data has already been acquired before
-        dev1_hash = hash(y[:10].tostring())
-        if dev1_hash == self.dev1_hash:
+        if len(self.x_data) == 0:
             return
-        self.dev1_hash = dev1_hash
 
-        # evaluating the processing string supplied to __init__
-        processed = eval(self.processing)
-
-        self.x_data.append(param2_val)
-        self.y_data.append(processed)
+        len_diff = len(self.unprocessed_data) - len(self.y_data)
+        if len_diff == 0:
+            return
+        for idx in reversed(range(len_diff)):
+            # self.processing string contains y which is then evaluated
+            y = self.unprocessed_data[-idx - 1]
+            self.y_data = np.append(self.y_data, eval(self.processing))
--- drivers/HistogramPlotterNormalized.py
+++ drivers/HistogramPlotterNormalized.py
@@ -1,13 +1,34 @@
 import logging
 import time
-import warnings
+from typing import Union
 
 import numpy as np
+import numpy.typing as npt
 from scipy.stats import binned_statistic
 
 
 def split(string, separator=","):
     return [x.strip() for x in string.split(separator)]
+
+
+def create_bins(
+    scan_values: Union[npt.NDArray[np.float_], npt.NDArray[np.int_]], maxsize: int = 100
+) -> tuple[npt.NDArray[np.float_], npt.NDArray[np.float_]]:
+    bin_centers = np.unique(scan_values)
+    if len(bin_centers) > maxsize:
+        bin_centers = np.linspace(bin_centers.min(), bin_centers.max(), maxsize)
+        bins = bin_centers.copy()
+        bin_width = bins[1] - bins[0]
+        bins -= bin_width / 2
+        bins = np.append(bins, bins.max() + bin_width / 2)
+
+    else:
+        dscan_min = np.diff(bin_centers).min()
+        bins = np.append(
+            [bin_centers.min() - dscan_min / 2],
+            bin_centers + dscan_min / 2,
+        )
+    return bin_centers, bins
 
 
 class HistogramPlotterNormalized:
@@ -28,10 +49,9 @@ class HistogramPlotterNormalized:
             self.processingnorm,
             self.dev2,
             self.param2,
-            self.lower,
-            self.higher,
-            self.width,
+            self.nbins_max,
         ) = params
+        self.nbins_max = int(self.nbins_max)
 
         # Need the " marks surrounding the expression otherwise the CeNTREX DAQ
         # enter_cmd fails due to the eval inside main, this is a workaround
@@ -40,37 +60,28 @@ class HistogramPlotterNormalized:
         self.dev2 = self.Strip(self.dev2, '"')
         self.param1 = self.Strip(self.param1, '"')
         self.param2 = self.Strip(self.param2, '"')
-        self.paramnorm = self.Strip(self.paramnorm, '"')
         self.processing = self.Strip(self.processing, '"')
+        self.paramnorm = self.Strip(self.paramnorm, '"')
         self.processingnorm = self.Strip(self.processingnorm, '"')
 
-        # lower and higher bounds for the bin range
-        self.lower = float(self.lower)
-        self.higher = float(self.higher)
-        # bin width
-        self.width = float(self.width)
-
-        self.verification_string = "test"
-
-        # lists to store matched slow (x) and processed fast (y) data in
+        self.verification_string = "histogram_plotter"
+        # lists to store matched slow (x), fast (unprocessed_data) and processed fast
+        # (y) data in
+        self.x_ts = []
         self.x_data = []
+        self.unprocessed_data_ts = []
+        self.unprocessed_data = []
+        self.unprocessed_data_norm = []
         self.y_data = []
-
-        # storing a hash to ensure only new fast data is added to the histogram
-        self.dev1_hash = None
-
-        # creating the bin edges
-        self.SetBins()
 
         self.warnings = []
         self.new_attributes = []
 
         # shape and type of the array of returned data
-        self.shape = (1, 2, len(self.bins) - 1)
+        self.shape = (1, 2, self.nbins_max)
         self.dtype = float
 
-        self.std_cutoff = 2
-        self.no_data_err = False
+        self.processed_changed = False
 
     def __enter__(self):
         return self
@@ -99,69 +110,31 @@ class HistogramPlotterNormalized:
         self.ProcessData()
         x_data = self.x_data
         y_data = np.array(self.y_data)
-        bins = self.bins
 
         # return zeros if no data present
-        if (len(x_data)) == 0:
+        if len(x_data) == 0 or len(y_data) == 0 or len(np.unique(x_data)) <= 1:
             data = np.concatenate(
                 (np.linspace(-1, 1, self.shape[-1]), np.zeros(self.shape[-1]))
             ).reshape(self.shape)
             return [data, [{"timestamp": time.time() - self.time_offset}]]
 
-        if len(y_data) == 0:
-            data = np.concatenate(
-                (bins[:-1] + self.width / 2, np.zeros(self.shape[-1]))
-            ).reshape(self.shape)
-            return [data, [{"timestamp": time.time() - self.time_offset}]]
+        else:
+            bin_centers, bins = create_bins(x_data, maxsize=self.nbins_max)
+            self.shape = (1, 2, len(bin_centers))
+            bin_means, bin_edges, bin_number = binned_statistic(
+                x_data, y_data, statistic="mean", bins=bins
+            )
+            data = np.concatenate((bin_centers, bin_means)).reshape(self.shape)
 
-        try:
-            if np.diff(bins)[0] <= 0:
-                data = np.concatenate(
-                    (np.linspace(-1, 1, self.shape[-1]), np.zeros(self.shape[-1]))
-                ).reshape(self.shape)
-                return [data, [{"timestamp": time.time() - self.time_offset}]]
-
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", category=RuntimeWarning)
-                try:
-                    bin_stds, bin_edges, bin_number = binned_statistic(
-                        x_data, y_data, statistic="std", bins=bins
-                    )
-                    dat = np.ones(len(bin_stds))*np.nan
-                    for idx, bin_std in zip(np.unique(bin_number), bin_stds):
-                        m = bin_number == idx
-                        bin_data = y_data[m]
-                        m = (
-                                (bin_data >= (bin_data.mean() - self.std_cutoff*bin_std)) & 
-                                (bin_data <= (bin_data.mean() + self.std_cutoff*bin_std))
-                        )
-                        try:
-                            dat[idx-1] = np.nanmean(bin_data[m])
-                        except IndexError:
-                            continue
-                    data = np.concatenate((self.bin_centers, dat)).reshape(self.shape)
-                except Exception as e:
-                    logging.error(f"Error in HistogramPlotterNormalized: {e}")
-                    dat, bin_edges, bin_number = binned_statistic(
-                        x_data, y_data, statistic="mean", bins=bins
-                    )
-                    data = np.concatenate((self.bin_centers, dat)).reshape(self.shape)
-
-            return [data, [{"timestamp": time.time() - self.time_offset}]]
-        except Exception as e:
-            logging.error(f"Error in HistogramPlotterNormalized: {e}")
-            data = np.concatenate(
-                (np.linspace(-1, 1, self.shape[-1]), np.zeros(self.shape[-1]))
-            ).reshape(self.shape)
             return [data, [{"timestamp": time.time() - self.time_offset}]]
 
     def SetProcessing(self, processing):
         self.processing = processing
-        self.ClearData()
+        self.processed_changed = True
 
     def SetProcessingNorm(self, processingnorm):
         self.processingnorm = processingnorm
-        self.ClearData()
+        self.processed_changed = True
 
     def SetDevice1(self, dev1):
         self.dev1 = dev1
@@ -183,41 +156,16 @@ class HistogramPlotterNormalized:
         self.param2 = param2
         self.ClearData()
 
-    def SetLower(self, lower):
-        self.lower = lower
-        self.SetBins()
-
-    def SetHigher(self, higher):
-        self.higher = higher
-        self.SetBins()
-
-    def SetWidth(self, width):
-        self.width = width
-        self.SetBins()
+    def set_nbins_max(self, nbins_max: int):
+        self.nbins_max = int(nbins_max)
 
     def ClearData(self):
+        self.x_ts = []
         self.x_data = []
+        self.unprocessed_data_ts = []
+        self.unprocessed_data = []
         self.y_data = []
-        self.dev1_hash = None
-        self.no_data_err = False
-
-    def SetBins(self):
-        if self.lower >= self.higher:
-            logging.error(
-                "Error in HistogramPlotterNormalized: lower bin is larger than higher bin"
-            )
-            return
-        bins = np.arange(self.lower, self.higher + self.width, self.width)
-        if len(bins) > 1_000:
-            logging.error(
-                "Error in HistogramPlotterNormalized: number of bins exceeds 1000"
-            )
-            self.bins = np.arange(-10,11,1)
-            self.bin_centers = self.bins[:-1] + 1/2
-            return
-        self.bins = bins
-        self.bin_centers = self.bins[:-1] + self.width / 2
-        self.shape = (1, 2, len(self.bins) - 1)
+        self.unprocessed_data_norm
 
     #################################################
     # Device Commands
@@ -228,68 +176,120 @@ class HistogramPlotterNormalized:
         Attempting to fetch data from the specified fast and slow device.
         """
         try:
-            data1 = self.parent.devices[self.dev1].config["plots_queue"][-1]
-            data2 = self.parent.devices[self.dev2].config["plots_queue"][-1]
-        except IndexError:
+            data1_queue = self.parent.devices[self.dev1].config["plots_queue"]
+        except KeyError:
+            logging.warning(f"HistogramPlotterNorm: device {self.dev1} not found")
+            return
+        try:
+            data2_queue = np.asarray(
+                self.parent.devices[self.dev2].config["plots_queue"]
+            )
+        except KeyError:
+            logging.warning(f"HistogramPlotterNorm: device {self.dev} not found")
             return
 
+        if len(data2_queue) == 0 or len(data1_queue) == 0:
+            return
+
+        timestamps1 = np.asarray([d[-1][0]["timestamp"] for d in data1_queue])
+        timestamps2 = np.asarray([d[0] for d in data2_queue])
+
+        dt = timestamps2[:, np.newaxis] - timestamps1
+        dt[dt > 0] = 1e3
+        indices2 = np.argmin(np.abs(dt), axis=0)
+
+        timestamps2 = timestamps2[indices2]
+        data2_queue = data2_queue[indices2]
+
+        if len(self.unprocessed_data) == 0:
+            mask = np.ones(len(data1_queue), dtype=bool)
+        else:
+            mask = timestamps1 > self.unprocessed_data_ts[-1]
+
         # extract the desired parameter 1 and 2
+        col_names1 = split(
+            self.parent.devices[self.dev1].config["attributes"]["column_names"]
+        )
+        col_names2 = split(
+            self.parent.devices[self.dev2].config["attributes"]["column_names"]
+        )
         try:
-            col_names1 = split(
-                self.parent.devices[self.dev1].config["attributes"]["column_names"]
-            )
-        except Exception as e:
-            logging.warning(f"Error in HistogramPlotterNormalized: {e}")
-            return
-        try:
-            col_names2 = split(
-                self.parent.devices[self.dev2].config["attributes"]["column_names"]
-            )
-        except Exception as e:
-            logging.warning(f"Error in HistogramPlotterNormalized: {e}")
-            return
-        try:
-            param1_dset = data1[0][0, col_names1.index(self.param1)].astype(float)
-            param1_norm_dset = data1[0][0, col_names1.index(self.paramnorm)].astype(
-                float
-            )
+            idx1 = col_names1.index(self.param1)
+            idx1norm = col_names1.index(self.paramnorm)
         except IndexError:
-            logging.error("Error in HistogramPlotterNormalized: param not found: " + self.param1)
+            logging.error("Error in HistogramPlotter: param not found: " + self.param1)
             return
         try:
-            param2_val = float(data2[col_names2.index(self.param2)])
+            idx2 = col_names2.index(self.param2)
         except IndexError:
-            logging.error("Error in HistogramPlotterNormalized: param not found: " + self.param2)
+            logging.error("Error in HistogramPlotter: param not found: " + self.param2)
             return
-        return param1_dset, param1_norm_dset, param2_val
+
+        self.unprocessed_data_ts = np.append(
+            self.unprocessed_data_ts, timestamps1[mask]
+        )
+        self.x_ts = np.append(self.x_ts, timestamps2[mask])
+
+        data1_queue = [data1_queue[idx] for idx, m in enumerate(mask) if m]
+        if len(self.unprocessed_data) == 0:
+            self.unprocessed_data = np.asarray(
+                [d[0][0][idx1] for d in data1_queue]
+            ).astype(float)
+            self.unprocessed_data_norm = np.asarray(
+                [d[0][0][idx1norm] for d in data1_queue]
+            ).astype(float)
+            self.x_data = np.asarray([d[idx2] for d in data2_queue[mask]])
+        else:
+            for idd, d in enumerate(data1_queue):
+                # d is a list with at 0 the arrays and at 1 the timestamps
+                d = d[0]
+                if d.shape[0] > 1:
+                    for di in d:
+                        self.unprocessed_data = np.vstack(
+                            [self.unprocessed_data, di[0][idx1]]
+                        ).astype(float)
+                        self.unprocessed_data_norm = np.vstack(
+                            [self.unprocessed_data_norm, di[0][idx1norm]]
+                        ).astype(float)
+                        self.x_data = np.append(
+                            self.x_data, data2_queue[mask][idd][idx2]
+                        ).astype(float)
+                else:
+                    self.unprocessed_data = np.vstack(
+                        [self.unprocessed_data, d[0][idx1]]
+                    ).astype(float)
+                    self.unprocessed_data_norm = np.vstack(
+                        [self.unprocessed_data_norm, d[0][idx1norm]]
+                    ).astype(float)
+                    self.x_data = np.append(
+                        self.x_data, data2_queue[mask][idd][idx2]
+                    ).astype(float)
 
     def ProcessData(self):
         """
         Processing data from the fast device
         """
-        data = self.FetchData()
-        if data is None:
-            if not self.no_data_err:
-                logging.warning(
-                    "Warning in HistogramPlotter ProcessData() : no data retrieved"
-                )
-                self.no_data_err = True
+        self.FetchData()
+
+        if self.processed_changed:
+            self.y_data = []
+            for y, y_norm in zip(self.unprocessed_data, self.processed_data_norm):
+                yi = eval(self.processing)
+                yin = eval(self.processingnorm)
+                self.y_data.append(yi / yin)
+            self.processed_changed = False
             return
-        self.no_data_err = False
 
-        y, y_norm, param2_val = data
-
-        # checking if fast device data has already been acquired before
-        dev1_hash = hash(y[:10].tostring())
-        if dev1_hash == self.dev1_hash:
+        if len(self.x_data) == 0:
             return
-        self.dev1_hash = dev1_hash
 
-        # evaluating the processing string supplied to __init__
-        # processing has to contain y
-        # processingnorm has to contain y_norm
-        processed = eval(self.processing)
-        processed_norm = eval(self.processingnorm)
-
-        self.x_data.append(param2_val)
-        self.y_data.append(processed / processed_norm)
+        len_diff = len(self.unprocessed_data) - len(self.y_data)
+        if len_diff == 0:
+            return
+        for idx in reversed(range(len_diff)):
+            # self.processing string contains y which is then evaluated
+            y = self.unprocessed_data[-idx - 1]
+            y_norm = self.unprocessed_data_norm[-idx - 1]
+            self.y_data = np.append(
+                self.y_data, eval(self.processing) / eval(self.processingnorm)
+            )

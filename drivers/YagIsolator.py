2a6354853529301106e8fb8b34899644112e4403
Olivier Grasdijk <o.grasdijk@gmail.com>
2023-11-03 11:20:02 -0500

    Update YagIsolator.py

--- drivers/YagIsolator.py
+++ drivers/YagIsolator.py
@@ -1,13 +1,15 @@
-import time
-import pyvisa
 import logging
+import time
+
+import pyvisa
+
 
 class YagIsolator:
     def __init__(self, time_offset, resource_name):
         self.time_offset = time_offset
         self.rm = pyvisa.ResourceManager()
 
-        if resource_name != 'client':
+        if resource_name != "client":
             try:
                 self.instr = self.rm.open_resource(resource_name)
             except pyvisa.errors.VisaIOError as err:
@@ -28,18 +30,18 @@ class YagIsolator:
         self.new_attributes = []
 
         # shape and type of the array of returned data
-        self.dtype = 'f'
-        self.shape = (1, )
+        self.dtype = "f"
+        self.shape = (1,)
 
         self.warnings = []
 
-        if resource_name == 'client':
+        if resource_name == "client":
             self.qswitch_status = False
             self.nr_qswitches = 0
         else:
             # convenience attributes to cut down on serial communication for
             # monitoring commands
-            self.Enable('C')
+            # self.Enable('C')
             self.flashlamp_status = bool(self.instr.query("status C"))
             self.qswitch_status = bool(self.instr.query("status D"))
             self.nr_qswitches = 0
@@ -52,9 +54,7 @@ class YagIsolator:
             self.instr.close()
 
     def ReadValue(self):
-        return [
-                time.time()-self.time_offset
-               ]
+        return [time.time() - self.time_offset]
 
     def GetWarnings(self):
         warnings = self.warnings
@@ -71,18 +71,18 @@ class YagIsolator:
     ##########           GUI COMMANDS                      ##########
     #################################################################
     def DisableQswitchGUI(self):
-        self.Disable('D')
+        self.Disable("D")
         self.qswitch_status = bool(self.instr.query("status D"))
 
     def EnableQswitchGUI(self):
-        self.Enable('D')
+        self.Enable("D")
         self.qswitch_status = bool(self.instr.query("status D"))
 
     def QswitchStatusGUI(self):
         if self.qswitch_status:
-            return 'enabled'
+            return "enabled"
         else:
-            return 'disabled'
+            return "disabled"
 
     def SetNrQswitchesGUI(self, nr_qswitches):
         self.NrQswitches(nr_qswitches)
@@ -90,7 +90,6 @@ class YagIsolator:
 
     def GetNrQswitchesGUI(self):
         return self.nr_qswitches
-
 
     #################################################################
     ##########           SERIAL COMMANDS                   ##########
@@ -100,36 +99,35 @@ class YagIsolator:
         try:
             return self.instr.query("*IDN?")
         except pyvisa.errors.VisaIOError as err:
-            logging.warning("YagIsolator warning in QueryIdentification(): "
-                            + str(err))
+            logging.warning("YagIsolator warning in QueryIdentification(): " + str(err))
             return str(err)
 
     def NrQswitches(self, nr_qswitches):
         """
         Let the qswitch through the isolator 'nr_qswitches' times
         """
-        cmd = f'qswitch {int(nr_qswitches)}'
+        cmd = f"trigger {int(nr_qswitches)}"
         ret = self.instr.query(cmd)
         if ret != cmd:
-            logging.warning("YagIsolator warning in QswitchCounter(): "+ret)
+            logging.warning("YagIsolator warning in NrQswitches(): " + ret)
 
     def Enable(self, channel):
-        assert channel in ['C', 'D'], f'invalid channel : {channel}'
-        cmd = f'enable {channel}'
+        assert channel in ["C", "D"], f"invalid channel : {channel}"
+        cmd = f"enable {channel}"
         ret = self.instr.query(cmd)
         if ret != cmd:
-            logging.warning("YagIsolator warning in Enable(): "+ret)
+            logging.warning("YagIsolator warning in Enable(): " + ret)
 
     def Disable(self, channel):
-        assert channel in ['C', 'D'], f'invalid channel : {channel}'
-        cmd = f'disable {channel}'
+        assert channel in ["C", "D"], f"invalid channel : {channel}"
+        cmd = f"disable {channel}"
         ret = self.instr.query(cmd)
         if ret != cmd:
-            logging.warning("YagIsolator warning in Disable(): "+ret)
+            logging.warning("YagIsolator warning in Disable(): " + ret)
 
 
 if __name__ == "__main__":
-    resource_name = input('specify resource name : ')
+    resource_name = input("specify resource name : ")
     yag = YagIsolator(time.time(), resource_name)
     print(yag.verification_string)
     for _ in range(9):

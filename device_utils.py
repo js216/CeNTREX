3ea500783d3d384b12ddcfdcbee962d4ce97eda3
ograsdijk <o.grasdijk@gmail.com>
2024-05-02 00:43:30 -0500

    add dropdown selection to the sequencer

--- device_utils.py
+++ device_utils.py
@@ -4,7 +4,7 @@ from device import Device
 def get_device_methods(device: str, devices: dict[str, Device]) -> list[str]:
     methods = [
         method
-        for method in devices[device].config.driver_class.__dict__
+        for method in devices[device].config["driver_class"].__dict__
         if method[:2] != "__"
     ]
     return methods
